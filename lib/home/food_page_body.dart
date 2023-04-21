1ec16ca401577081bd028d63db167abba0b7ff43
Oladisea <ladisea55@gmail.com>
2023-04-21 10:56:11 +0100

    Initial commit

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  _FoodPageBodyState createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      child: PageView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+        return _buildPageItem(index);
+      }),
+    );
+  }
+}
+Widget _buildPageItem(int position){
+  return Container(
+    height: 220,
+    margin: EdgeInsets.only(left: 5,right: 5),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.blue
+    ),
+  );
+}
