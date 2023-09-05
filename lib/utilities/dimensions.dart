720c24ee20eb8914a251d721e856e9387f4cb714
OLADISEA <ladisea55@gmail.com>
2023-09-05 20:43:44 +0100

    Added Dimensions class for managing responsive layout dimensions

---
+++ lib/utilities/dimensions.dart
@@ -0,0 +1,58 @@
+import 'package:get/get.dart';
+
+class Dimensions{
+  //screen height is 1096
+  // screen width is 500
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  //sizes
+  static double pageView = screenHeight/3.425;//320
+  static double sized = screenHeight/36.533;//30
+  static double small = screenHeight/109.6;//10
+  static double padding = screenHeight/73.0667;//15
+
+  // widths
+  static double width10 = screenHeight/109.6;//10
+  static double width20 = screenHeight/54.8;//20
+  static double width30 = screenHeight/36.533;//30
+  static double width45 = screenHeight/24.356;//45
+
+  // Heights
+  static double height20 = screenHeight/54.8;//20
+  static double height45 = screenHeight/24.356;//45
+  static double height30 = screenHeight/36.533;//30
+  static double height10 = screenHeight/109.6;//10
+  static double height100 = screenHeight/10.96;//100
+
+  //font size
+  static double font20 = screenHeight/54.8;
+  static double font12 = screenHeight/91.33;
+  static double font26 = screenHeight/42.15;//26
+  static double font16 = screenHeight/68.5;//16
+
+
+  //margin and padding
+  static double paddingRight15 = screenHeight/73.0667;//15
+  static double bottom15 = screenHeight/73.0667;//15
+  static double right20 = screenHeight/54.8;//20
+  static double left20 = screenHeight/54.8;//20
+
+  //icon sizes
+  static double iconSize24 = screenHeight/45.67;
+
+  // radius
+  static double radius30 = screenHeight/36.533;//30
+  static double radius20 = screenHeight/36.533;//20
+  static double radius15 = screenHeight/73.0667;//15
+
+
+  // Container sizes
+  static double firstContainer = screenHeight/4.882;//220
+  static double pageView140 = screenHeight/7.82857;//140
+
+  //popular food
+  static double popularFoodHeight = screenHeight/3.1413;
+
+
+}
