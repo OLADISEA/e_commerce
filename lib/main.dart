f2f4deaae752e2c5cdc4d51f620edb11dec73682
OLADISEA <ladisea55@gmail.com>
2023-09-09 23:50:24 +0100

    Initialized and set up the Flutter application with dependencies, fetching popular and recommended product data using GetX controllers.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-import 'package:e_commerce/home/main_food_page.dart';
+import 'package:e_commerce/controllers/popular_product_controllers.dart';
+import 'package:e_commerce/controllers/recommended_product_controllers.dart';
+import 'package:e_commerce/routes/page_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'Pages/home/main_food_page.dart';
+import 'services/dependencies.dart' as dep;
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
@@ -11,13 +18,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    Get.find<PopularProductController>().getPopularProductList();
+    Get.find<RecommendedProductController>().getRecommendedProductList();
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainFoodPage()
+      home: const MainFoodPage(),
+      initialRoute: PageRoutes.initial,
+      getPages: PageRoutes.routes,
     );
   }
 }
